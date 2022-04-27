eabfc5952cbad0021b8afe6ebc6ac1d2158e4e83
Brock lebeau <brocklebeau338gmail.com>
2022-04-26 22:08:56 -0400

    Added reviews controller

--- backend/api/restaurants.routes.js
+++ backend/api/restaurants.routes.js
@@ -1,8 +1,15 @@
 import express from "express";
 import RestaurantsCtrl from "./restaurants.controller.js";
+import ReviewsCtrl from "./reviews.controller.js";
 
 const router = express.Router();
 
 router.route("/").get(RestaurantsCtrl.apiGetRestaurants);
+
+router
+    .route('/review')
+    .post(RestaurantsCtrl.apiPostReview)
+    .put(ReviewsCtrl.appUpdateReview)
+    .delete(ReviewsCtrl.apiDeleteReview)
 
 export default router;
